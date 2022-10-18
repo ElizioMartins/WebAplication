9ed43c994319dc1d543e23fe17a73171af34d1d7
ElizioMartins <elizio.martins@hotmail.com>
2022-10-18 15:14:33 -0300

    preparando para refatoração

--- WebAplication/src/app/app-routing.module.ts
+++ WebAplication/src/app/app-routing.module.ts
@@ -1,24 +1,22 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
-
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const routes: Routes = [
-  { path: 'cursos', component: CursosComponent},
-  { path: 'curso/:id', component: CursoDetalheComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
-  { path: '', component: HomeComponent}
+  { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+  { path: '', component: HomeComponent },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

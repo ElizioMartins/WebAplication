87e58392c16b44f7be227241e294277a69b20eac
ElizioMartins <elizio.martins@hotmail.com>
2022-10-18 14:41:05 -0300

    Antes de refatora o modulo de rotas

--- WebAplication/src/app/app-routing.module.ts
+++ WebAplication/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
 
 const routes: Routes = [
   { path: 'cursos', component: CursosComponent},
-  { path: 'cursos/:id', component: CursosComponent},
+  { path: 'curso/:id', component: CursoDetalheComponent},
   { path: 'login', component: LoginComponent},
+  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
   { path: '', component: HomeComponent}
 ];
 

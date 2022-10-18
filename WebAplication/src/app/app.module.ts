7ea45d00de849d088eaf1f986ea6ff846e35d02f
ElizioMartins <elizio.martins@hotmail.com>
2022-10-18 15:45:43 -0300

    Module e routing module

--- WebAplication/src/app/app.module.ts
+++ WebAplication/src/app/app.module.ts
@@ -3,27 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CursosComponent,
-    HomeComponent,
-    LoginComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [CursosService],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule, CursosModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
